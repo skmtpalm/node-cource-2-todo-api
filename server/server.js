5cbb4076d780abd5828949e7313a105d5df89710
skmtpalm <hachihachi.8og@gmail.com>
2017-04-25 15:32:58 +0900

    Add user model

--- server/server.js
+++ server/server.js
@@ -5,13 +5,18 @@ mongoose.connect('mongodb://localhost:27017/TodoApp');
 
 var Todo = mongoose.model('Todo', {
   text: {
-    type: String
+    type: String,
+    required: true,
+    minlength: 1,
+    trim: true
   },
   completed: {
-    type: Boolean
+    type: Boolean,
+    default: false
   },
   completedAt: {
-    type: Number
+    type: Number,
+    default: null
   }
 });
 
@@ -25,14 +30,37 @@ var Todo = mongoose.model('Todo', {
 //   console.log('Unable to save todo', e);
 // });
 
-var otherTodo = new Todo({
-  text: 'Feed the cat',
-  completed: true,
-  completedAt: 123
+// var otherTodo = new Todo({
+//   text: 'Something to do'
+// });
+// //
+// otherTodo.save().then((doc) => {
+//   console.log(JSON.stringify(doc, undefined, 2));
+// }, (e) => {
+//   console.log('Unable to save', e);
+// });
+
+
+//User
+// email - require it  - trim it - set type - set min length of 1
+// name
+
+var User = mongoose.model('User', {
+  email: {
+    type: String,
+    required: true,
+    trim: true,
+    minlength: 1
+  }
 });
 
-otherTodo.save().then((doc) => {
+var newUser = new User({
+  email: 'hachihachi.8og@gmail.com   '
+});
+
+
+newUser.save().then((doc) => {
   console.log(JSON.stringify(doc, undefined, 2));
 }, (e) => {
-  console.log('Unable to save', e);
+  console.log('Unable to save user', e)
 });
