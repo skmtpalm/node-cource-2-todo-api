0435d374b3ee4bbc92e89b71a1624a9922b12e65
skmtpalm <hachihachi.8og@gmail.com>
2017-04-25 17:28:58 +0900

    Add GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var {ObjectID} = require('mongodb');
+
 
 var { mongoose } = require('./db/mongoose');
 
@@ -29,6 +31,24 @@ app.get('/todos', (req, res) => {
   }, (e) => {
     res.status(400).send(e);
   });
+});
+
+// GET /todos/12345566
+app.get('/todos/:id', (req, res) => {
+  var id = req.params.id;
+
+  if (!ObjectID.isValid(id)) {
+    res.status(404).send()
+  }
+
+  Todo.findById(id).then((todo) => {
+    if (!todo) {
+      return res.status(404).send();
+    }
+    res.send({todo});
+  }).catch((e) => {
+    res.status(400).send()
+  })
 });
 
 app.listen(3000, () => {
